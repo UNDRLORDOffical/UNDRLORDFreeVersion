fe4f83faf803cb001489df29e43b4faf237f6112
UNDRLORDOffical <123339199+UNDRLORDOffical@users.noreply.github.com>
2023-01-30 18:18:15 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,1 +1,4 @@
-#UNDRLORD On Top!
+--All Credit to : Brizzy#9999 , MinWasTaken#2873
+--Discord:  https://discord.gg/cNJsFEzA
+
+loadstring(game:HttpGet('https://scripts.luawl.com/14387/jailbreakautoarrest.lua'))()
